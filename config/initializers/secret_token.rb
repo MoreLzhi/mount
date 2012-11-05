d39d0e09e47c03ad48a35cc07799729e59eb7fe1
Alex Naumov <alexsnaumov@gmail.com>
2012-11-05 19:55:45 +0700

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mountwiki::Application.config.secret_token = 'f96ef8d3d749ee44f767477d20a7c05dda7658245e8a3ddabe85bd648293ddd09fa8042872671ad21ab3fea85d5244d6e1e240f2787820d47c128f8a56632728'
